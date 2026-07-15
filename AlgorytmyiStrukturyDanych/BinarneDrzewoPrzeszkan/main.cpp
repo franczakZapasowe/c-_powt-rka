1a619a404f8bc0b132cc6c1b29d9b1e268ab1a62
franczakZapasowe <russeldurantmvp@gmail.com>
2026-07-15 21:08:02 +0200

    Implementacja Binarnego drzewa przeszukan (BST)

---
+++ AlgorytmyiStrukturyDanych/BinarneDrzewoPrzeszkan/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+struct Wezel {
+    int wartosc;
+    Wezel *lewy;
+    Wezel *prawy;
+};
+
+bool serch(Wezel *w, int x) {
+    if (w == nullptr) return false;
+    if (x == w->wartosc) return true;
+
+    if (x < w->wartosc) return serch(w->lewy, x);
+    return serch(w->prawy, x);
+}
+
+    void insert(Wezel *&w, int x) {
+        if (w==nullptr) {
+            w = new Wezel;
+            w->wartosc = x;
+            w->lewy = nullptr;
+            w->prawy = nullptr;
+        }else {
+            if (x < w->wartosc) insert(w->lewy, x);
+            else insert(w->prawy, x);
+        }
+    }
+
+void usun() {
+
+}
+
+
+int main() {}
