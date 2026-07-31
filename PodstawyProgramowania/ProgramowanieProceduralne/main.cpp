0715d318b140f7463ed4e17109bf4a6d9b29aeeb
franczakZapasowe <russeldurantmvp@gmail.com>
2026-07-31 19:11:48 +0200

    Zadanie 1 - skonczone

--- PodstawyProgramowania/ProgramowanieProceduralne/main.cpp
+++ PodstawyProgramowania/ProgramowanieProceduralne/main.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 constexpr int ROZMIAR = 1000;
+int iloscElementowGlobal = 0;
+int nGlobal = 0;
+int licznikWywolanPetli;
 
 enum class Stany {
     CzekamNaDane = 0,
@@ -14,7 +17,7 @@ enum class Stany {
 };
 
 enum class KodBledu {
-    ZaMaloArgumentow,ZaDuzaWartosc,ObaBledy,BrakBedu
+    ZaMaloArgumentow,ZaDuzaWartosc,BrakBedu
 };
 
 struct DaneSterujace {
@@ -32,23 +35,29 @@ void StanWypisz (DaneSterujace& stan);
 void StanZgloscBlad(DaneSterujace& stan);
 void Reset (DaneSterujace& stan);
 //funkcje pomocnicze
-int ileArgumentow();
-int pobierzDane();
 void wypisz();
 
+/*
+ * WYMYŚLIŁEM TAKIE ZASADY DZIALANIA PROGRAMU ZE PROGRAM DZIALA GDY ARGC > 1 I WYNIK
+ * PRZY MNOZENIU ARGV * LICZBE 0-5 <5000
+ */
 int main(int argc, char  *argv[]) {
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<int> dist(0, 10);
+    uniform_int_distribution<int> dist(0, 5);
 
+    iloscElementowGlobal = argc;
     cout<<"Podano: "<<argc<<" parametrow\n";
     for (int i =0; i<argc; i++) cout<<i<<": "<<argv[i]<<endl;
     cout<<"n:\n";
+    int suma = 0;
     if (argc>1) {
-        int n = atoi(argv[1]);
-        cout<<n*dist(gen)<<endl;
+        for (int i =0; i<argc ;i++) {
+            nGlobal+= atoi(argv[i]);
+        }
     }
+    nGlobal *= dist(gen);
 
     DaneSterujace sterowanie;
     sterowanie.atualyStan = Stany::CzekamNaDane;
@@ -62,24 +71,24 @@ int main(int argc, char  *argv[]) {
         Reset
     };
 
-    while (sterowanie.dziala) {
+    // z racji na zalozenia zadania - dane zganriam z argumentow main - czyli to dzieje sie raz tylko to jezeli obliczona wartosc bedzie wieksza niz 5000 to petla bedzie dzialacc w nieskonczosc
+    // wiec rbie zabespieczenie ze 100
+    while (sterowanie.dziala && licznikWywolanPetli<100) {
         dane [(int)sterowanie.atualyStan] (sterowanie);
-
+        licznikWywolanPetli++;
     }
-
     return 0;
 }
 
-
-
 void StanCzekaniaNaDane (DaneSterujace& stan) {
-    cout<<"Czkeam na dane z systemu:\n";
+    cout<<"STAN CZEKANIA NA DANE Z SYSTEMU:\n";
     stan.atualyStan = Stany::SprawdzDane;
 }
 
 void StanSprawdzDane (DaneSterujace& stan) {
-    int n  =  ileArgumentow();
-    int x = pobierzDane();
+    cout<<"STAN SPRWADZANIA DANYCH:\n";
+    int n  = iloscElementowGlobal;
+    int x = nGlobal;
 
     if (n > 1 && x < 1000)  {
         stan.atualyStan = Stany::Wypisz;
@@ -91,42 +100,36 @@ void StanSprawdzDane (DaneSterujace& stan) {
         stan.blad = KodBledu::ZaMaloArgumentow;
         stan.atualyStan = Stany::ZglosBlad;
     }
-    else if (n < 1 &&  x> 1000) {
-        stan.blad = KodBledu::ObaBledy;
-        stan.atualyStan = Stany::ZglosBlad;
-    }
 }
+
 void StanZgloscBlad(DaneSterujace& stan) {
+    cout<<"STAN SPRAWDZANIA BLEDOW:\n";
     cout<<"Blad\n";
     switch (stan.blad) {
         case KodBledu::ZaMaloArgumentow:
             cout<<"Podano za malo arguemtnow\n";
             break;
         case KodBledu::ZaDuzaWartosc:
-            cout<<"Z obliczen wyszla za duza wartosc\n";
-            break;
-        case KodBledu::ObaBledy:
-            cout<<"Oba bledy\n";
+            cout<<"Z obliczen wyszla za duza wartosc(n>5000)\n";
             break;
     }
-    stan.atualyStan= Stany::Reset;
+    stan.atualyStan = Stany::Reset;
 }
 
 void StanWypisz (DaneSterujace& stan) {
+    cout<<"STAN WYPISYWANIA DANYCH:\n";
     wypisz();
     stan.dziala = false;
 }
 
 void Reset (DaneSterujace& stan) {
-    cout<<"Reset Danych sterujacych\n";
+    cout<<"STAN RESET\n";
     stan.blad = KodBledu::BrakBedu;
     stan.atualyStan = Stany::CzekamNaDane;
 }
 
-int pobierzDane() {
-
-}
-
 void wypisz() {
-
+    cout<<"Ilosc elementow podanych: "<<iloscElementowGlobal<<endl;
+    cout<<"Wynik: "<<nGlobal<<endl;
 }
+
