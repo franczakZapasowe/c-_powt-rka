ff522a691fc65375e28414c6c7914c5feac432c1
franczakZapasowe <russeldurantmvp@gmail.com>
2026-08-06 19:25:14 +0200

    Projekt AQC-Core

--- ZawansowanePodstawyPogramowanieProceduralneDynamicznaAlokacjaPamieciOperacjePlikoweBitowe/ZadanieProjekt/main.cpp
+++ ZawansowanePodstawyPogramowanieProceduralneDynamicznaAlokacjaPamieciOperacjePlikoweBitowe/ZadanieProjekt/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdint>
 #include <cstring>
+#include <filesystem>
 #include <random>
 constexpr int ODCHYLENIEWYMIAROWESTALE = 100;
 namespace rekord {
@@ -16,8 +17,8 @@ namespace rekord {
 }
 
 uint64_t dodajRekord(uint64_t,uint64_t,uint64_t,uint64_t);
-void edytujRekod(uint64_t&,int);
-bool  weryfiukujRekord(uint64_t&rekord);
+void edytujRekod(uint64_t& r,FILE*p);
+bool  weryfiukujRekord(uint64_t rekord);
 
 int main() {
     std::random_device rd;
@@ -25,19 +26,42 @@ int main() {
     std::uniform_int_distribution<> distrib(0, 2000000);
     std::uniform_int_distribution<> distrib2(0, 6000);
     std::uniform_int_distribution<> distrib3(0, 65535);
-    int rozmiarBzowy = 50;
-    uint64_t *rejestrWadliwych = new uint64_t[rozmiarBzowy];
-    for (int i=0;i<rozmiarBzowy;i++) {
-        //do skonczoneia
-        rejestrWadliwych[i] = dodajRekord(distrib(rd),distrib2(rd),distrib3(gen),0b01011110);
+    uint64_t *wszystkieDane = new uint64_t[50];
+
+    FILE * plik = fopen("wszystkiedane.bin","w+b");
+
+    // za
+    for (int i=0;i<50;i++) {
+        wszystkieDane[i] = dodajRekord(distrib(rd),distrib2(rd),distrib3(gen),0b01011110);
+        fwrite(&wszystkieDane[i],sizeof(wszystkieDane[i]),1,plik);
     }
-    FILE * plik = fopen("wszystkiedane.txt","w+b");
-    FILE * plik2 = fopen("poprawneDane.txt","w+b");
-    int i = 0;
-    while (!feof(plik)) {
-        // Zarządzanie Pamięcią Operacyjną (RAM)
-    int rozmiarSprawdzany = sizeof(rejestrWadliwych)/sizeof(rejestrWadliwych[0]);
-        if (rozmiarBzowy - rozmiarSprawdzany == 1) {
+    delete []wszystkieDane;
+
+    FILE * plikTymczasowy = fopen("poprawneDane.bin","w+b");
+    FILE * plikFinalny = fopen("final.txt","w");
+    int rozmiarBzowy = 5;
+    uint64_t *rejestrWadliwych = new uint64_t[rozmiarBzowy];
+
+    int licznikWczytanych = 0;
+    uint64_t rekordPojedynczy = 0;
+    rewind(plik);
+    while (true) {
+
+        // odczytuje element z pliku 1 i zapisuje je do tablicy
+
+        fread(&rekordPojedynczy,sizeof(uint64_t),1,plik);
+        if (feof(plik)) break;
+        if (weryfiukujRekord(rekordPojedynczy)) {
+            // jezeli pomyslnie przejdzie weryfikacje to mzienamy go w orginalnym pliku jak nie to zapisuemy to do tablicy wadliwych
+            fseek(plik,-sizeof(rejestrWadliwych[0]),SEEK_CUR);
+            edytujRekod(rekordPojedynczy,plik);
+        }else {
+            rejestrWadliwych[licznikWczytanych] = rekordPojedynczy;
+            licznikWczytanych++;
+        }
+
+        // mechanizm odpowiedzalny za zwiekszanie rozmiaru tablicy
+        if (rozmiarBzowy - licznikWczytanych == 1) {
             int rozmiarTymczasowy = rozmiarBzowy * 2;
             uint64_t * rejestTymczasowy = new uint64_t [rozmiarTymczasowy];
 
@@ -45,25 +69,37 @@ int main() {
 
             delete[] rejestrWadliwych;
             rejestrWadliwych = rejestTymczasowy;
-            rejestrWadliwych = nullptr;
+            rejestTymczasowy = nullptr;
             rozmiarBzowy = rozmiarTymczasowy;
         }
-
-        fread(&rejestrWadliwych[i],sizeof(uint64_t),1,plik);
-        fwrite(&rejestrWadliwych[i],sizeof(uint64_t),1,plik2);
-        if (weryfiukujRekord(rejestrWadliwych[i])) {
-            fseek(plik,-sizeof(rejestrWadliwych[0]),SEEK_CUR);
-        }
-        i++;
     }
 
     //zapis do nowego pliku
-    for (int i=0;i<rozmiarBzowy;i++) {
-        fprintf(plik2,"%d;\n",rejestrWadliwych[i]);
+    for (int i=0;i<licznikWczytanych;i++) {
+        //do dokonczenias
+        uint64_t nr, odchylenie,temperatura,bledy;
+        uint64_t maskaNr = 1048575;
+        uint64_t maskaOdchyleni = 4095;
+        uint64_t maskaTemperatura = 65535;
+        uint64_t maskaStatus = 255;
+
+        nr = rejestrWadliwych[i];
+        odchylenie = rejestrWadliwych[i]>>20;
+        temperatura = rejestrWadliwych[i]>>32;
+        bledy = rejestrWadliwych[i]>>48;
+
+        nr &= maskaNr;
+        odchylenie &= maskaOdchyleni;
+        temperatura &= maskaTemperatura;
+        bledy &= maskaStatus;
+
+        fprintf(plikFinalny,"Nr.%llu;Odchylenie:%llu;temperatura%llu;bledy%llu;\n", nr,odchylenie,temperatura,bledy);
     }
 
     fclose(plik);
-    fclose(plik2);
+    fclose(plikFinalny);
+    fclose(plikTymczasowy);
+
     delete[] rejestrWadliwych;
 }
 
@@ -86,22 +122,38 @@ uint64_t dodajRekord(uint64_t nr,uint64_t odchylenie,uint64_t temperatura,uint64
     rekod = nr | odchylenie | temperatura | status ;
     return rekod ;
 }
-bool weryfiukujRekord(uint64_t& rekord) {
-    uint64_t tempDlaOdchylenia = rekord;
-    rekord = (rekord>>48);
-    uint64_t maska = 0b01111111;
+bool weryfiukujRekord(uint64_t rekord) {
+     //ODCHYLENIEWYMIAROWESTALE
+    uint64_t rekordStatusow = rekord;
+    uint64_t rekordBledow = rekord;
+    uint64_t rekordOdchylenie = rekord;
+    uint64_t rekodFlagiInspekcji = rekord;
 
-    uint64_t maskaOdchyleni = 4095;
-    tempDlaOdchylenia  = (tempDlaOdchylenia>>20);
-    tempDlaOdchylenia &=maskaOdchyleni;
+    uint64_t maskaSzczelonosciIObwodow = 0b00000011;
+    uint64_t maskaOdchylenie = 4095;
+    uint64_t maskaBledow = 0b01111000;
+    uint64_t maskaInspekcji = 1;
 
-    if ((maska&rekord) && (tempDlaOdchylenia > ODCHYLENIEWYMIAROWESTALE)) {
-        //zmiana bitu flagi nie jest juz podejrzany
-        // zmiana flagi przetworzenia
-        uint64_t maskaFlagi = 0b10000100;
-        rekord ^= maskaFlagi;
-        rekord = (rekord<<48);
+    rekordStatusow = rekordStatusow >> 48;
+    rekordOdchylenie = rekordOdchylenie >> 20;
+    rekordBledow = rekordBledow>>48;
+    rekodFlagiInspekcji = rekodFlagiInspekcji>>50;
+
+
+
+    uint64_t testJedn = rekordStatusow & maskaSzczelonosciIObwodow;
+    uint64_t test2 = rekordBledow &= maskaBledow;
+    uint64_t test3 = maskaInspekcji&rekodFlagiInspekcji;
+
+
+    if ((testJedn == 3 ) &&  ((rekordOdchylenie&maskaOdchylenie) < ODCHYLENIEWYMIAROWESTALE || test3 == 0) && (test2==0))  {
         return true;
     }
+
     return false;
+}
+void edytujRekod(uint64_t& r,FILE*p) {
+    uint64_t maska = 1ULL<<55;
+    r|=maska;
+    fwrite(&r,sizeof(r),1,p);
 }
