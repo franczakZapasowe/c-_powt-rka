9016425048a1ac5c7ac521416299d67ff34d5880
franczakZapasowe <russeldurantmvp@gmail.com>
2026-08-22 14:37:38 +0200

    zadanie filesystem

--- ProgramowanieGeneryczne/ZdanieFileSystem/main.cpp
+++ ProgramowanieGeneryczne/ZdanieFileSystem/main.cpp
@@ -55,8 +55,28 @@ int main() {
 
     std::filesystem::path newPath = curent/"AudioBackup";
     std::filesystem::create_directories(newPath);
+    for (auto &erntry: std::filesystem::recursive_directory_iterator{curentAudio}) {
+        if (erntry.path().extension() == ".txt") {
+            std::string pomocznicza = erntry.path().parent_path().filename().string();
+            std::string curentFIleName = erntry.path().filename().string();
+            std::string fullNewName = pomocznicza + "_" + curentFIleName;
+            std::filesystem::path destinationPath = newPath / fullNewName;
+            std::filesystem::copy_file(erntry.path(),destinationPath, std::filesystem::copy_options::overwrite_existing);
+        }
+    }
 
-    
+    FILE * plik = fopen("raport.txt", "w");
+    if (plik == nullptr) {
+        std::cout<<"Nie mozna otworzyc plik raport.txt\n";
+        return 0;
+    }
 
-
+    for (auto &entry : std::filesystem::recursive_directory_iterator{newPath}) {
+        if (entry.path().extension() == ".txt") {
+            auto size = std::filesystem::file_size(entry.path());
+            std::string path = entry.path().string();
+            fprintf(plik,"Scizeka: %s, rozmiar: %ju\n",path.c_str(),size );
+        }
+    }
+    fclose(plik);
 }
